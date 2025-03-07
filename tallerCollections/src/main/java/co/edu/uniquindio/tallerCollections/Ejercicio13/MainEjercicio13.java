8a9a0165a6b92731a14f2687a681f2bfcc380e9f
Natalia Ramírez <83674618+Nataramirez@users.noreply.github.com>
2025-03-06 23:32:10 -0500

    :construction: Ejercicios 13 y 17

---
+++ tallerCollections/src/main/java/co/edu/uniquindio/tallerCollections/Ejercicio13/MainEjercicio13.java
@@ -0,0 +1,32 @@
+package co.edu.uniquindio.tallerCollections.Ejercicio13;
+
+import co.edu.uniquindio.tallerCollections.Ejercicio13.modelos.Paciente;
+import co.edu.uniquindio.tallerCollections.Ejercicio13.enums.Prioridad;
+
+import java.util.PriorityQueue;
+
+public class MainEjercicio13 {
+    public static void main(String[] args) {
+        PriorityQueue<Paciente> turnos = new PriorityQueue<>();
+
+        Paciente paciente1 = new Paciente("natalia", Prioridad.ALTA);
+        Paciente paciente2 = new Paciente("sara", Prioridad.BAJA);
+        Paciente paciente3 = new Paciente("valentina", Prioridad.MEDIA);
+        Paciente paciente4 = new Paciente("robinson", Prioridad.ALTA);
+        turnos.add(paciente1);
+        turnos.add(paciente2);
+        turnos.add(paciente3);
+        turnos.add(paciente4);
+
+        System.out.print("Orden de turnos: \n");
+        for (Paciente turno : turnos) {
+            System.out.print(turno.getNombre() + "\n" );
+        }
+
+        Paciente turnoSiguiente1 = turnos.poll();
+        System.out.println("Atender Paciente: " + turnoSiguiente1.getNombre());
+
+        Paciente turnoSiguiente2 = turnos.poll();
+        System.out.println("Atender Paciente: " + turnoSiguiente2.getNombre());
+    }
+}
